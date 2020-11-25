thehyve/cbioportal

Add indexes for queried fields to schema 2.12.7

db-scripts/src/main/resources/migration.sql
@@ -904,7 +904,8 @@ CREATE TABLE `alteration_driver_annotation` (
   `DRIVER_TIERS_FILTER_ANNOTATION` VARCHAR(80),
   PRIMARY KEY (`ALTERATION_EVENT_ID`, `GENETIC_PROFILE_ID`, `SAMPLE_ID`),
   FOREIGN KEY (`GENETIC_PROFILE_ID`) REFERENCES `genetic_profile` (`GENETIC_PROFILE_ID`) ON DELETE CASCADE,
-  FOREIGN KEY (`SAMPLE_ID`) REFERENCES `sample` (`INTERNAL_ID`) ON DELETE CASCADE
+  FOREIGN KEY (`SAMPLE_ID`) REFERENCES `sample` (`INTERNAL_ID`) ON DELETE CASCADE,
+  INDEX (`DRIVER_FILTER`)
 ) COMMENT='Alteration driver annotation';
 insert into `alteration_driver_annotation`
 select `INTERNAL_ID`, `GENETIC_PROFILE_ID`, `SAMPLE_ID`, `DRIVER_FILTER`, `DRIVER_FILTER_ANNOTATION`, `DRIVER_TIERS_FILTER`, `DRIVER_TIERS_FILTER_ANNOTATION` from `structural_variant`
@@ -928,4 +929,6 @@ drop column `DRIVER_FILTER`,
 drop column `DRIVER_FILTER_ANNOTATION`,
 drop column `DRIVER_TIERS_FILTER`,
 drop column `DRIVER_TIERS_FILTER_ANNOTATION`;
+CREATE INDEX idx_mutation_type ON mutation_event (`MUTATION_TYPE`);
+CREATE INDEX idx_cna_type ON cna_event (`ATERATION`);
 UPDATE `info` SET `DB_SCHEMA_VERSION`="2.12.7";

violethaze74/Bioinformatic-Portal

db-scripts/src/main/resources/migration.sql
@@ -904,7 +904,8 @@ CREATE TABLE `alteration_driver_annotation` (
   `DRIVER_TIERS_FILTER_ANNOTATION` VARCHAR(80),
   PRIMARY KEY (`ALTERATION_EVENT_ID`, `GENETIC_PROFILE_ID`, `SAMPLE_ID`),
   FOREIGN KEY (`GENETIC_PROFILE_ID`) REFERENCES `genetic_profile` (`GENETIC_PROFILE_ID`) ON DELETE CASCADE,
-  FOREIGN KEY (`SAMPLE_ID`) REFERENCES `sample` (`INTERNAL_ID`) ON DELETE CASCADE
+  FOREIGN KEY (`SAMPLE_ID`) REFERENCES `sample` (`INTERNAL_ID`) ON DELETE CASCADE,
+  INDEX (`DRIVER_FILTER`)
 ) COMMENT='Alteration driver annotation';
 insert into `alteration_driver_annotation`
 select `INTERNAL_ID`, `GENETIC_PROFILE_ID`, `SAMPLE_ID`, `DRIVER_FILTER`, `DRIVER_FILTER_ANNOTATION`, `DRIVER_TIERS_FILTER`, `DRIVER_TIERS_FILTER_ANNOTATION` from `structural_variant`
@@ -928,4 +929,6 @@ drop column `DRIVER_FILTER`,
 drop column `DRIVER_FILTER_ANNOTATION`,
 drop column `DRIVER_TIERS_FILTER`,
 drop column `DRIVER_TIERS_FILTER_ANNOTATION`;
+CREATE INDEX idx_mutation_type ON mutation_event (`MUTATION_TYPE`);
+CREATE INDEX idx_cna_type ON cna_event (`ALTERATION`);
 UPDATE `info` SET `DB_SCHEMA_VERSION`="2.12.7";